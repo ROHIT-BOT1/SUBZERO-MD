3f39feb4c7126cf63dee481d5328c0452d20a807
ROHIT-BOT1 <dasrakish77@gmail.com>
2025-08-01 00:15:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;zdUUTCaa#7jnzxQm7EgVbbWNzBPfW-OzqocHoSYnFhJtSO_hlq8U", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
